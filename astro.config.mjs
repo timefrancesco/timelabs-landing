a7ede1613727561fd24896aebc1dcb9c466372ae
Francesco Pretelli <francesco@timelabs.io>
2026-02-06 10:12:09 +0900

    seo optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://timelabs.io',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
